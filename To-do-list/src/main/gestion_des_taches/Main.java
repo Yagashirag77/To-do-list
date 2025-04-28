d5021bfa6f70c9c459c12453fd2de66f2ca830f8
Yagashirag77 <yassine.gartite2004@gmail.com>
2025-04-28 07:46:35 +0100

    Delete To-do-list directory

--- To-do-list/src/main/gestion_des_taches/Main.java
+++
@@ -1,7 +0,0 @@
-package main.gestion_des_taches;
-
-public class Main {
-    public static void main(String[] args) {
-        // TODO: Launch JavaFX App
-    }
-}
--- To-do-list/src/main/gestion_des_taches/controller/ProjetController.java
+++
@@ -1,3 +0,0 @@
-package com.todoapp.controller;
-
-public class ProjetController {}
